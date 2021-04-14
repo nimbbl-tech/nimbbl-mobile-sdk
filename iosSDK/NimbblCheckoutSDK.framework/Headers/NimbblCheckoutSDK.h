9f88d843d5fe5048812fc3d8eb023dd83ea33468
Stany Dsouza <stany.dsouza@mswipe.in>
2021-04-14 14:38:11 +0530

    ios adk folder created

---
+++ iosSDK/NimbblCheckoutSDK.framework/Headers/NimbblCheckoutSDK.h
@@ -0,0 +1,19 @@
+//
+//  NimbblCheckoutSDK.h
+//  NimbblCheckoutSDK
+//
+//  Created by Stany on 08/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NimbblCheckoutSDK.
+FOUNDATION_EXPORT double NimbblCheckoutSDKVersionNumber;
+
+//! Project version string for NimbblCheckoutSDK.
+FOUNDATION_EXPORT const unsigned char NimbblCheckoutSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NimbblCheckoutSDK/PublicHeader.h>
+
+
+#import <NimbblCheckoutSDK/NimbblCheckoutViewController.h>
